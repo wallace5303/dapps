a4f745a6e7ccea5531403748b224baa0b6d3fe04
gaoshuaixing <gsx@zhemai.com>
2019-10-25 10:53:38 +0800

    1.0.6

--- app/service/store.js
+++ app/service/store.js
@@ -479,7 +479,7 @@ class StoreService extends BaseService {
     );
     let downloadType = 'github';
     if (this.app.config.env === 'prod') {
-      downloadType = 'gitee';
+      downloadType = 'coding';
     }
     await tools.wget(appPath, appid, downloadType);
 
